4df0947ea31ff68356ae1396856509745c58c1a5
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-24 11:56:28 -0500

    added test methods

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -7,4 +7,37 @@ import static org.junit.Assert.*;
 
 public class TestMain {
 
+//    @Test
+//    public void testCodeupString() {
+//        assertEquals("Codeup", "CodeUp"); // Test fail
+//        assertNotEquals("Codeup", "CodeUp"); // Test pass
+//    }
+
+//    @Test
+//    public void testNotSameArrayList() {
+//        List<String> languages = new ArrayList<>();
+//        List<String> moreLanguages = new ArrayList<>();
+//
+//        assertNotSame( languages, moreLanguages); // Test pass
+//        assertSame(languages, moreLanguages); // Test fail
+//    }
+
+//    @Test
+//    public void testIfArraysAreEqual() {
+//        int[] number = {1, 2, 3}; // Array
+//        int[] otherNumbers = new int[3];
+//        otherNumbers[0] = 1;
+//        otherNumbers[1] = 2;
+//        otherNumbers[2] = 3;
+//
+//        assertArrayEquals(number, otherNumbers); // Test pass
+//    }
+
+//    @Test
+//    public void testCharactersInString() {
+//        String language = "PHP";
+//        assertTrue(language.contains("H")); // Test pass
+//        assertFalse(language.contains("J")); // Test pass
+//    }
+
 }
